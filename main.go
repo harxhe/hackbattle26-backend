9f3db760077946f9fce32ca66012ef71b208e584
Harsh Vardhan <harshvardhan.p27@gmail.com>
2026-09-14 20:55:25 +0530

    cors updated for redefine (#26)
    * cors updated for redefine
    
    * fix formatting

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	routes.RegisterAuthRoutes(router, config.AuthClient, config.FirestoreClient)
 	routes.RegisterTeamRoutes(router)
 	
-	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:3000", "http://localhost:3001", "http://localhost:3002","https://hackbattle-26-frontend.vercel.app","https://hackbattle.ieeecsvit.com","https://hackbattle-26-frontend-rry2.vercel.app","hackbattle.ieeecsvit.com"})
+	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:3000", "http://localhost:3001", "http://localhost:3002","https://redefine-26-frontend.vercel.app","https://redefine.ieeecsvit.com"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
 	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
 	allowCredentials := handlers.AllowCredentials()
